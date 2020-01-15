0c37f66b565aec5e3fdfab6061aa9502fd775fcb
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2020-01-15 02:03:11 -0300

    Define some useful consts

--- Joystick.h
+++ Joystick.h
@@ -53,27 +53,39 @@
 #include "Descriptors.h"
 
 // Type Defines
-// Enumeration for joystick buttons.
-typedef enum {
-	SWITCH_Y       = 0x01,
-	SWITCH_B       = 0x02,
-	SWITCH_A       = 0x04,
-	SWITCH_X       = 0x08,
-	SWITCH_L       = 0x10,
-	SWITCH_R       = 0x20,
-	SWITCH_ZL      = 0x40,
-	SWITCH_ZR      = 0x80,
-	SWITCH_SELECT  = 0x100,
-	SWITCH_START   = 0x200,
-	SWITCH_LCLICK  = 0x400,
-	SWITCH_RCLICK  = 0x800,
-	SWITCH_HOME    = 0x1000,
-	SWITCH_CAPTURE = 0x2000,
-} JoystickButtons_t;
+#define SWITCH_NONE    0x0
+#define	SWITCH_Y       0x01
+#define	SWITCH_B       0x02
+#define	SWITCH_A       0x04
+#define	SWITCH_X       0x08
+#define	SWITCH_L       0x10
+#define	SWITCH_R       0x20
+#define	SWITCH_ZL      0x40
+#define	SWITCH_ZR      0x80
+#define	SWITCH_MINUS   0x100
+#define	SWITCH_PLUS    0x200
+#define	SWITCH_LCLICK  0x400
+#define	SWITCH_RCLICK  0x800
+//#define	SWITCH_HOME    0x1000
+//#define	SWITCH_CAPTURE 0x2000
+
+#define HAT_TOP          0x00
+#define HAT_TOP_RIGHT    0x01
+#define HAT_RIGHT        0x02
+#define HAT_BOTTOM_RIGHT 0x03
+#define HAT_BOTTOM       0x04
+#define HAT_BOTTOM_LEFT  0x05
+#define HAT_LEFT         0x06
+#define HAT_TOP_LEFT     0x07
+#define HAT_CENTER       0x08
+
+#define STICK_MIN      0
+#define STICK_CENTER 128
+#define STICK_MAX    255
 
 // Joystick HID report structure. We have an input and an output.
 typedef struct {
-	uint16_t Button; // 16 buttons; see JoystickButtons_t for bit mapping
+	uint16_t Button; // 16 buttons; see SWITCH_* consts for bit mapping
 	uint8_t  HAT;    // HAT switch; one nibble w/ unused nibble
 	uint8_t  LX;     // Left  Stick X
 	uint8_t  LY;     // Left  Stick Y
@@ -85,7 +97,7 @@ typedef struct {
 // The output is structured as a mirror of the input.
 // This is based on initial observations of the Pokken Controller.
 typedef struct {
-	uint16_t Button; // 16 buttons; see JoystickButtons_t for bit mapping
+	uint16_t Button; // 16 buttons; see SWITCH_* consts for bit mapping
 	uint8_t  HAT;    // HAT switch; one nibble w/ unused nibble
 	uint8_t  LX;     // Left  Stick X
 	uint8_t  LY;     // Left  Stick Y
